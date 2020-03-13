a5dffd278402e10f0da73d71dd31be5c3e8d87a5
Daya Khudia <dskhudia@fb.com>
2020-03-12 20:57:24 -0700

    Make spmmfp32 and spmmi8 benchmark consistent with tests (#315)
    Summary:
    Pull Request resolved: https://github.com/pytorch/FBGEMM/pull/315
    
    Making SPMM fp32 and int8 benchmarks consistent with tests to minimize confusion between M, N and K. Also this way the benchmark is consistent with FC op implementation in Caffe2.
    
    Reviewed By: jiecaoyu
    
    Differential Revision: D20400801
    
    fbshipit-source-id: 1408985cb1ba24af1bfee9dbc0b6418758bb06f6

--- bench/SpMMFP32Benchmark.cc
+++ bench/SpMMFP32Benchmark.cc
@@ -13,13 +13,12 @@ using namespace std;
 using namespace fbgemm;
 
 int main(int, char**) {
-  vector<char> llc(128 * 1024 * 1024);
 
-  vector<vector<int>> shapes = {{1024, 128, 1024}};
+  vector<vector<int>> shapes = {{128, 1024, 1024}};
 
   // C is MxN -> CT is NxM
-  // A is MxK -> BT is KxM
-  // B is KxN -> AT is NxK
+  // A is MxK -> AT is KxM
+  // B is KxN -> BT is NxK
 
   // for (int s = 64; s <= 128; s *= 2)
   for (auto const& s : shapes) {
@@ -27,41 +26,57 @@ int main(int, char**) {
     int n = s[1];
     int k = s[2];
 
-    int lda = k;
-    int ldb = n;
-    int ldc = n;
-
     for (float fnz = 0.99; fnz >= 0.009999; fnz -= 0.01) {
-      auto aData = getRandomSparseVector(m * k, fnz);
-      auto bData = getRandomSparseVector(k * n);
+      auto aData = getRandomSparseVector(m * k);
+      auto bData = getRandomSparseVector(k * n, fnz);
       auto cData = getRandomSparseVector(m * n);
 
-      auto fn = generateSpMM<float>(m, n, k, aData.data(), lda, ldb, ldc);
-      auto fn_varying_n = generateSpMM<float>(m, k, aData.data(), lda);
+      aligned_vector<float> atData(k * m);
+      aligned_vector<float> btData(n * k);
+      aligned_vector<float> ctData(n * m);
+
+      transpose_matrix(m, k, aData.data(), k, atData.data(), m);
+      transpose_matrix(k, n, bData.data(), n, btData.data(), k);
+
+      // We calculate C^T = B^T x A^T
+      // B matrix is sparse and passed in as first matrix to generateSpMM
+      int ldat = m;
+      int ldbt = k;
+      int ldct = m;
+      auto fn = generateSpMM<float>(n, m, k, btData.data(), ldbt, ldat, ldct);
+      auto fn_varying_n = generateSpMM<float>(n, k, btData.data(), ldbt);
 
       double effective_flop = m * n * k * 2;
 
       constexpr int NWARMUP = 5;
       constexpr int NITER = 32;
       auto secs = measureWithWarmup(
-          [&]() { fn(bData.data(), cData.data(), 0); },
+          [&]() { fn(atData.data(), ctData.data(), 0); },
           NWARMUP,
           NITER,
-          [&]() { llc_flush(llc); });
+          [&]() {
+            cache_evict(atData);
+            cache_evict(btData);
+            cache_evict(ctData);
+          });
 
       auto secs_varying_n = measureWithWarmup(
           [&]() {
             fn_varying_n(
-                bData.data(),
-                cData.data(),
-                n,
-                n, /* ldb */
-                n, /* ldc */
+                atData.data(),
+                ctData.data(),
+                m,
+                ldat, /* ldat */
+                ldct, /* ldct */
                 0 /* accum_flag */);
           },
           NWARMUP,
           NITER,
-          [&]() { llc_flush(llc); });
+          [&]() {
+            cache_evict(atData);
+            cache_evict(btData);
+            cache_evict(ctData);
+          });
 
       double effective_gflops = effective_flop / secs / 1e9;
       double effective_gflops_varying_n = effective_flop / secs_varying_n / 1e9;
--- bench/SpMMI8Benchmark.cc
+++ bench/SpMMI8Benchmark.cc
@@ -14,62 +14,89 @@ using namespace std;
 using namespace fbgemm;
 
 int main(int, char**) {
-  vector<char> llc(128 * 1024 * 1024);
 
-  // vector<vector<unsigned>> shapes = {{64, 64, 64}};
-  // vector<vector<unsigned>> shapes = {{1, 16, 4}};
-
-  vector<vector<unsigned>> shapes = {{1024, 128, 1024}};
+  vector<vector<unsigned>> shapes = {{128, 1024, 1024}};
 
   // C is MxN -> CT is NxM
-  // A is MxK -> BT is KxM
-  // B is KxN -> AT is NxK
+  // A is MxK -> AT is KxM
+  // B is KxN -> BT is NxK
 
-  // for (unsigned s = 64; s <= 128; s *= 2)
   for (auto const& s : shapes) {
     int m = s[0];
     int n = s[1];
     int k = s[2];
 
-    int lda = k;
-    int ldb = n;
-    int ldc = n;
+    if ((k % 4) != 0) {
+      cout << "Skipping shape " << m << ", " << n << ", " << k;
+      cout << " as K is not a multiple of 4" << endl;
+      continue;
+    }
 
     for (float fnz = 0.99; fnz >= 0.009999; fnz -= 0.01) {
-      auto aData = getRandomSparseVector(m * k / 4, fnz);
-      auto bData = getRandomSparseVector(k * n / 4);
+      auto aData = getRandomSparseVector(m * k / 4);
+      auto bData = getRandomSparseVector(k * n / 4, fnz);
       auto cData = getRandomSparseVector(m * n);
 
-      auto aptr = reinterpret_cast<const int8_t*>(aData.data());
-      auto bptr = reinterpret_cast<uint8_t*>(bData.data());
-
-      for (int i = 0; i < k * n; ++i) {
-        bptr[i] &= 0x7F;
-      }
-
+      auto aptr = reinterpret_cast<uint8_t*>(aData.data());
+      auto bptr = reinterpret_cast<const int8_t*>(bData.data());
       auto cptr = reinterpret_cast<int32_t*>(cData.data());
 
-      auto fn = generateSpMM<int32_t>(m, n, k, aptr, lda, ldb, ldc);
-      auto fn_varying_n = generateSpMM<int32_t>(m, k, aptr, lda);
+      for (int i = 0; i < k * m; ++i) {
+        aptr[i] &= 0x7F;
+      }
+
+      // We calculate C^T = B^T x A^T
+      // B matrix is sparse and passed in as first matrix to generateSpMM
+      int ldat = m;
+      int ldbt = k;
+      int ldct = m;
+
+      aligned_vector<float> atData(k / 4 * m);
+      aligned_vector<float> btData(n * k);
+      aligned_vector<float> ctData(n * m);
+
+      auto atptr = reinterpret_cast<const uint8_t*>(atData.data());
+      auto btptr = reinterpret_cast<int8_t*>(btData.data());
+      auto ctptr = reinterpret_cast<int32_t*>(ctData.data());
+
+      // Transpose as if A is float so 4 columns are interleaved
+      transpose_matrix(m, k / 4, aData.data(), k / 4, atData.data(), ldat);
+      transpose_matrix(k, n, bptr, n, btptr, ldbt);
+
+      auto fn = generateSpMM<int32_t>(n, m, k, btptr, ldbt, ldat, ldct);
+      auto fn_varying_n = generateSpMM<int32_t>(n, k, btptr, ldbt);
 
       double FLOPs = m * n * k * 2;
 
       constexpr int NWARMUP = 5;
       constexpr int NITER = 32;
       auto secs = measureWithWarmup(
-          [&]() { fn(bptr, cptr, 0); },
+          [&]() { fn(atptr, ctptr, 0); },
           NWARMUP,
           NITER,
-          [&]() { llc_flush(llc); });
+          [&]() {
+            cache_evict(atData);
+            cache_evict(btData);
+            cache_evict(ctData);
+          });
 
       auto secs_varying_n = measureWithWarmup(
           [&]() {
             fn_varying_n(
-                bptr, cptr, n, n /* ldb */, n /* ldc */, 0 /* accum_flag */);
+                atptr,
+                ctptr,
+                m,
+                ldat /* ldb */,
+                ldct /* ldc */,
+                0 /* accum_flag */);
           },
           NWARMUP,
           NITER,
-          [&]() { llc_flush(llc); });
+          [&]() {
+            cache_evict(atData);
+            cache_evict(btData);
+            cache_evict(ctData);
+          });
 
       cout << fnz << "," << (FLOPs / secs / 1e9) << ","
            << (fnz * FLOPs / secs / 1e9) << ","
